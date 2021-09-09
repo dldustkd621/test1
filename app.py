87dd7b9d93228c254f15c5bf9531bcdb7bdddb10
unknown <dldustkd621@gmail.com>
2021-09-09 21:10:26 +0900

    'second'

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    url = request.form["urlBox"]
+    return render_template('index.html', data = url)
